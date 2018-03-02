77429bcc1eb593b90173b8ddb2b2455245c90e6e
fpinnock <35967650+fpinnock@users.noreply.github.com>
2018-03-02 00:45:18 -0500

    Add files via upload

--- src/Solve.jl
+++ src/Solve.jl
@@ -1,7 +1,12 @@
 include("Include.jl")
 
 # load the data dictionary -
-data_dictionary = DataDictionary(0,0,0)
+
+data_dictionary = no_regulation_aerobic_data_dictionary(0,0,0)
+#data_dictionary = no_regulation_anaerobic_data_dictionary(0,0,0)
+#data_dictionary = regulation_aerobic_data_dictionary(0,0,0)
+#data_dictionary = regulation_anaerobic_data_dictionary(0,0,0)
+
 
 # solve the lp problem -
-(objective_value, flux_array, dual_array, uptake_array, exit_flag, status_flag) = FluxDriver(data_dictionary)
+(objective_value, flux_array, dual_array, uptake_array, exit_flag) = FluxDriver(data_dictionary)
